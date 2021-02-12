ed2f90cd1128e815de3c99593d618d512699b036
ctaylor <ctaylr13@gmail.com>
2021-02-11 20:40:15 -0500

    react router added

--- app/client/src/components/App.js
+++ app/client/src/components/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Router, Route, Switch } from 'react-router-dom';
+import Home from './Home';
+import history from '../history';
 
 const App = () => {
-    return <div>Hello World</div>;
+    return (
+        <div>
+            <Router history={history}>
+                <div>
+                    <Route path="/" exact={true} component={Home} />
+                </div>
+            </Router>
+        </div>
+    );
 };
 
 export default App;
---
+++ app/client/src/components/Home.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class Home extends Component {
+    render() {
+        return <div>Home Component</div>;
+    }
+}
+
+export default Home;
