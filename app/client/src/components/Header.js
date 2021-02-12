e332c7b3ef7b5a7473cbc65c3eea757a15b61dfd
ctaylor <ctaylr13@gmail.com>
2021-02-12 12:00:08 -0500

    links w/ react router working

--- app/client/src/components/Header.js
+++ app/client/src/components/Header.js
@@ -3,17 +3,26 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import Link from '@material-ui/core/Link';
+import { Link } from 'react-router-dom';
+import TrickList from '../components/TrickList/TrickList';
 
 export default function Header() {
     return (
         <div>
             <AppBar position="static">
                 <Toolbar>
-                    <Button color="inherit">Home</Button>
-                    <Button color="inherit">Trick List</Button>
-                    <Button color="inherit">Practice</Button>
-                    <Button color="inherit">Random</Button>
+                    <Link to="/">
+                        <Button color="inherit">Home</Button>
+                    </Link>
+                    <Link to="/list">
+                        <Button color="inherit">Trick List</Button>
+                    </Link>
+                    <Link to="/practice">
+                        <Button color="inherit">Practice</Button>
+                    </Link>
+                    <Link to="random">
+                        <Button color="inherit">Random</Button>
+                    </Link>
                 </Toolbar>
             </AppBar>
         </div>
